b54fb11a4b0d00837749b14b1e175523b95239b1
vadivai <vaikanvn@gmail.com>
2023-12-09 13:38:28 +0200

    Изменил import App в index.js, работает!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { App } from './components/App/App';
+import { App } from 'components';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
